74bc11f57c034fc504d9e48aaf2ac29e4ca826b4
ayush-khandelwal <ayush.k.d@intimetec.com>
2024-08-08 16:17:15 +0530

    todo app init

---
+++ todo-app/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-app>
+        <v-main><h1>ToDo App</h1></v-main>
+    </v-app>
+</template>
+
+<script>
+export default {
+    name: "App",
+
+    components: {},
+
+    data: () => ({
+        //
+    }),
+};
+</script>
