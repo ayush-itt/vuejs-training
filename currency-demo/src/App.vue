024036d9033890cfd359fa7878068fef40d89be4
ayush-khandelwal <ayush.k.d@intimetec.com>
2024-08-28 15:10:12 +0530

    currency-demo added

---
+++ currency-demo/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-app>
+        <v-app-bar app color="primary" dark>
+            <div class="d-flex align-center">
+                <h1>USD to INR</h1>
+            </div>
+        </v-app-bar>
+
+        <v-main>
+            <CurrencyConverter
+                v-model="amountInUSD"
+                :exchangeRate="83.92"
+            ></CurrencyConverter>
+            <!-- <CurrencyConverter
+                :value="amountInUSD"
+                @input="amountInUSD = $event"
+                :exchangeRate="83.92"
+            ></CurrencyConverter> -->
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+import CurrencyConverter from "./components/CurrencyConverter.vue";
+
+export default {
+    name: "App",
+
+    components: {
+        CurrencyConverter,
+    },
+
+    data: () => ({
+        amountInUSD: 10,
+    }),
+};
+</script>
